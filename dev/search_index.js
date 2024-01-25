65e57abe3832bcbf67f0eb3c22ade252ad0fa1d2
Documenter.jl <documenter@juliadocs.github.io>
2024-01-25 21:29:59 +0000

    build based on 5e59ba7

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OceanGreensFunctionMethods","category":"page"},{"location":"#OceanGreensFunctionMethods","page":"Home","title":"OceanGreensFunctionMethods","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OceanGreensFunctionMethods.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OceanGreensFunctionMethods]","category":"page"}]
+}
