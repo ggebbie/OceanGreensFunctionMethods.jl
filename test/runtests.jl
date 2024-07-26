da68f1a3b39ad8b8853bea9d9e1699d5a93de214
G Jake Gebbie <ggebbie@whoi.edu>
2024-07-26 13:05:02 -0400

    tests to compare to `Distributions.InverseGaussian`

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,31 @@
+using Revise
 using OceanGreensFunctionMethods
+using Distributions
+using DynamicQuantities
+# using OrdinaryDiffEq, ModelingToolkit, MethodOfLines, DomadinSets, Unitful
+# using DrWatson
+# using Plots
 using Test
 
+#include("config_units.jl")
+
 @testset "OceanGreensFunctionMethods.jl" begin
-    # Write your tests here.
+
+    # compare a Tracer Inverse Gaussian distribution
+    # with geophysical arguments
+    # to an Inverse Gaussian with statistical arguments
+    Γ = 20.0 # mean
+    Δ = 20.0 # width
+
+    G = TracerInverseGaussian(Γ, Δ)
+
+    # should be identical to this Inverse Gaussian
+    G2 = InverseGaussian(mean(G),shape(G))
+
+    @test isequal(shape(G),shape(G2))
+    @test !isequal(params(G),params(G2))
+    @test isequal(partype(G), partype(G2))
+    @test isequal(mean(G), mean(G2))
+    @test isequal(var(G), var(G2))
+
 end
